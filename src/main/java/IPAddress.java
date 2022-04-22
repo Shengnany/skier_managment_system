8bda1e04a46c3b75c68e02cbfe953826d510c70e
shengnanyou <youshengnan1998@gmail.com>
2022-04-21 22:01:14 -0700

    first commit

---
+++ src/main/java/IPAddress.java
@@ -0,0 +1,7 @@
+public final class IPAddress {
+    private IPAddress() {}
+    public static final String RESORT_REDIS_IP = "35.167.62.214";
+    public static final String TOTAL_VERTICAL_IP = "52.38.94.127";
+    public static final String TOTAL_VERTICAL_LIST_IP = "52.40.247.194";
+    public static final String RMQ_IP = "35.88.60.103";
+}
